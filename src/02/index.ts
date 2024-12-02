8ac70cee755cd5f9b8417047676fdd360f6f4303
Marvin Frede <mfrede@slashwhy.de>
2024-12-02 08:08:37 +0100

    day-02a: solved

--- src/02/index.ts
+++ src/02/index.ts
@@ -2,4 +2,32 @@ import { read, readLines } from "../util/txt-reader";
 
 const lines = readLines("../02/input.txt");
 
-console.log(lines)
+let numberOfSafeRecords = 0;
+
+lines.forEach((line) => {
+  const numbersOfLine = line.split(/\s+/).map((s) => Number(s));
+  if (lineIsSave(numbersOfLine)) numberOfSafeRecords++;
+});
+
+function lineIsSave(line: number[]) {
+  let prevNumber: number | undefined = undefined;
+  let isAsc: boolean = false;
+  let isDesc: boolean = false;
+
+  for (let i = 1; i < line.length; i++) {
+    prevNumber = line[i - 1];
+    const currNumber = line[i];
+    if (Math.abs(currNumber - prevNumber) > 3) return false;
+    if (currNumber > prevNumber) {
+      isAsc = true;
+      if (isDesc) return false;
+    } else if (currNumber < prevNumber) {
+      isDesc = true;
+      if (isAsc) return false;
+    } else return false;
+  }
+
+  return true;
+}
+
+console.log("numberOfSafeRecords", numberOfSafeRecords);
