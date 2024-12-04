a06ba53609fe9ae566efb39e845ad733fec8c3e6
Marvin Frede <mfrede@slashwhy.de>
2024-12-04 07:19:53 +0100

    day-03b: solved

---
+++ src/03b/index.ts
@@ -0,0 +1,26 @@
+import { read, readLines } from "../util/txt-reader";
+
+const lines = readLines("../03b/input.txt");
+const line = lines.join("");
+
+const matcher = /don't\(\)|do\(\)|mul(\(((\d{1,3}),(\d{1,3})\)))/g;
+const matches = line.matchAll(matcher);
+
+let sum = 0;
+let mode: "do" | "dont" = "do";
+
+for (const element of matches) {
+  if (element[0][2] === "n") {
+    mode = "dont";
+  } else if (element[0][2] === "(") {
+    mode = "do";
+  } else {
+    if (mode === "do") {
+      const a = Number(element[3]);
+      const b = Number(element[4]);
+      sum += a * b;
+    }
+  }
+}
+
+console.log(sum);
